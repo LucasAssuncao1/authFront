98066e2f8528961db9440d95876996c557d3afe2
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-03-27 18:48:55 -0300

    protegento a rota admin quando não está autenticado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,21 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guard/auth.guard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () =>
-          import('./core/components/pages/auth/auth.component').then((m) => m.AuthComponent),
-      },
-      {
-        path: 'admin',
-        loadComponent: () =>
-          import('./components/admin/pages/admin/admin.component').then((m) => m.AdminComponent),
-      },
-      {
-        path: 'signin',
-        loadComponent: () =>
-          import('./core/components/pages/signin/signin.component').then((m) => m.SigninComponent),
-      }
-    ];
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/components/pages/auth/auth.component').then((m) => m.AuthComponent),
+  },
+  {
+    path: 'admin',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./components/admin/pages/admin/admin.component').then((m) => m.AdminComponent),
+  },
+  {
+    path: 'signin',
+    loadComponent: () =>
+      import('./core/components/pages/signin/signin.component').then((m) => m.SigninComponent),
+  }
+];
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,6 +3,9 @@ import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
 
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,24 +19,33 @@ export class AuthService {
   constructor() { }
 
   public signIn(payload: { email: string, password: string }): Observable<any> {
-    return this.http.post<{token: string}>(`${this.url}/sign`, payload).pipe(
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
       map((res) => {
         localStorage.removeItem('access_token');
         localStorage.setItem('access_token', res.token);
         return this.router.navigate(['admin']);
-        
+
       }),
       catchError((e) => {
-        if(e.error.message) return throwError(() =>  e.error.message);
+        if (e.error.message) return throwError(() => e.error.message);
 
-        return throwError(() =>  'Estamos com problemas no momento, tente mais tarde!');
+        return throwError(() => 'Estamos com problemas no momento, tente mais tarde!');
       })
     );
   }
 
-  public signOut(){
+  public signOut() {
     localStorage.removeItem('access_token');
     return this.router.navigate(['']);
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token');
+  
+    if (!token) return false; // Se não houver token, usuário não está autenticado
+  
+    const jwtHelper = new JwtHelperService();
+    return !jwtHelper.isTokenExpired(token); // Verifica se o token não está expirado
   }
 
 }
